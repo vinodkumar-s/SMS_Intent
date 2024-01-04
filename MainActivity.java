7d528ce67666cc7b614668af4029fbdfd3888257
Priyasenthil17 <130549990+Priyasenthil17@users.noreply.github.com>
2024-01-04 14:59:13 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.ex_5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button mbutton=(Button) findViewById(R.id.smsButton);
+        mbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) 
+          {
+                /*.................WRITE THE CODING PART FOR INTENT METHOD FOR SENDING SMS..............
+                ........................................................................................
+                ........................................................................................
+               .........................................................................................
+               */
+            }
+        });
+
+    }
+}
